266e37efd9de7f3b26f0ce8200c0de6888c33e39
gerry <gerry>
2023-06-10 22:19:45 -0700

    增加proto file，增加GameService

--- src/main/java/com/me/server/DieStreamingRequest.java
+++ src/main/java/com/me/server/DieStreamingRequest.java
@@ -1,2 +1,56 @@
-package com.me.server;public class DieStreamingRequest {
+package com.me.server;
+
+import com.me.game.Die;
+import com.me.game.GameState;
+import com.me.game.Player;
+import io.grpc.stub.StreamObserver;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class DieStreamingRequest implements StreamObserver<Die> {
+    private Player client;
+    private Player server;
+    private StreamObserver<GameState> gameStateStreamObserver;
+
+    public DieStreamingRequest(Player client, Player server, StreamObserver<GameState> gameStateStreamObserver) {
+        this.client = client;
+        this.server = server;
+        this.gameStateStreamObserver = gameStateStreamObserver;
+    }
+
+    @Override
+    public void onNext(Die die) {
+        client = getNewPlayerPosition(client, die.getValue());
+        if (client.getPosition() != 100) {
+            server = getNewPlayerPosition(server, ThreadLocalRandom.current().nextInt(1, 7));
+        }
+        gameStateStreamObserver.onNext(getGameState());
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+
+    }
+
+    @Override
+    public void onCompleted() {
+        gameStateStreamObserver.onCompleted();
+    }
+
+    private GameState getGameState() {
+        return GameState.newBuilder()
+                .addPlayer(client)
+                .addPlayer(server)
+                .build();
+    }
+
+    private Player getNewPlayerPosition(Player player, int dieValue) {
+        int position = player.getPosition() + dieValue;
+        if (position <= 100) {
+            player = player.toBuilder()
+                    .setPosition(position)
+                    .build();
+        }
+        return player;
+    }
 }
--- src/main/java/com/me/server/GameService.java
+++ src/main/java/com/me/server/GameService.java
@@ -3,11 +3,14 @@ package com.me.server;
 import com.me.game.Die;
 import com.me.game.GameServiceGrpc;
 import com.me.game.GameState;
+import com.me.game.Player;
 import io.grpc.stub.StreamObserver;
 
 public class GameService extends GameServiceGrpc.GameServiceImplBase {
     @Override
     public StreamObserver<Die> roll(StreamObserver<GameState> responseObserver) {
-        return super.roll(responseObserver);
+        Player client = Player.newBuilder().setName("client").setPosition(0).build();
+        Player server = Player.newBuilder().setName("server").setPosition(0).build();
+        return new DieStreamingRequest(client, server, responseObserver);
     }
 }
